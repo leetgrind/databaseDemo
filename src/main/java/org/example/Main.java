8026bd749af9817d9ca4b154a8979ec1552bb343
leetgrind <leetgrind.org@gmail.com>
2023-11-01 21:03:32 -0400

    Added select

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,16 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            Connection conn = DriverManager.
-                    getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres",
-                            "123456");
+        // try catch with resources
+
+        try(Connection conn = DriverManager.
+                getConnection("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres",
+                        "123456")) {
 
             if(conn!=null) {
                 System.out.println("Connected to the database");
@@ -23,17 +21,31 @@ public class Main {
 
             Statement statement = conn.createStatement();
 
-            String query = "create table student(id int primary key, name varchar(20));";
+            //String query = "create table student(id int primary key, name varchar(20));";
 
-            int result = statement.executeUpdate(query);
+            //String query = "INSERT INTO STUDENT VALUES(2, 'Amy');";
 
-            System.out.println("Query executed with result: " + result);
+            String query = "SELECT * FROM STUDENT;";
+
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+
+                int id = rs.getInt("ID");
+                String name = rs.getString("NAME");
+
+                System.out.println("ID: " + id + ", Name: " + name);
+
+            }
+
+            //int result = statement.executeUpdate(query);
+
+            //System.out.println("Query executed with result: " + result);
 
             statement.close();
-            conn.close();
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
